46bf205e43832512be447d7d66538f96578233f1
JABIRAN <ds6951@naver.com>
2024-07-24 18:15:37 +0900

    [commit] insert 구현중

--- src/main/java/com/climate/main/mapper/CommunityMapper.java
+++ src/main/java/com/climate/main/mapper/CommunityMapper.java
@@ -1,6 +1,7 @@
 package com.climate.main.mapper;
 
 import com.climate.main.dto.CommunityDTO;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -9,7 +10,15 @@ import java.util.List;
 @Mapper
 public interface CommunityMapper {
 
-    @Select("select bo.*, u_nickname from cli_board bo, cli_user where u_id = b_u_id and b_category = 'showoff' ")
-    public List<CommunityDTO> selectCommunityShowoff();
+    @Select("select bo.*, u_nickname, u_grade from cli_board bo, cli_user where u_id = b_u_id and b_category = '動画' ")
+    public List<CommunityDTO> selectAllCommunityShowoff();
 
+    @Select("select bo.*, u_nickname, u_grade from cli_board bo, cli_user where u_id = b_u_id and b_category in ('クルー募集','急遽') ")
+    public  List<CommunityDTO> selectAllCommunityRecruitment();
+
+    @Select("select bo.*, u_nickname, u_grade from cli_board bo, cli_user where u_id = b_u_id and b_pk = #{b_pk} ")
+    public  CommunityDTO selectCommunityShowoff(int b_pk);
+
+    @Insert("insert into cli_board values (cli_board_seq.nextval, 'ds6951', '動画', #{b_video}, #{b_title}, #{b_text}, sysdate)")
+    public int insertCommunityShowoff(CommunityDTO communityDTO);
 }
