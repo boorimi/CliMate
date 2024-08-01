a6795f94561d4319a4be6b029ade555541621484
boorimi <qnfal123@gmail.com>
2024-08-02 03:50:29 +0900

    [edit] community menu, lfg CSS, [add] community menu.js

---
+++ src/main/resources/static/js/community_menu.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    const searchOpenIcon = $(".community-search-icon");
+    const searchMenu = $(".community-search-wrapper");
+
+    searchOpenIcon.append('<img src="/resources/icon/search.png">');
+
+    // 검색 인풋 오픈
+    searchOpenIcon.click(function () {
+        searchMenu.slideToggle();
+        searchMenu.css("display", "flex");
+
+        const iconImg = searchOpenIcon.find('img');
+        // 아이콘 토글
+        if (iconImg.attr('src') === '/resources/icon/search.png') {
+            iconImg.attr('src', '/resources/icon/close.png');
+            iconImg.css('width', '50%');
+        } else {
+            iconImg.attr('src', '/resources/icon/search.png');
+            iconImg.css('width', '100%');
+        }
+    });
+
+});
