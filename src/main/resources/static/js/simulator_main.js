2180bbbaf774fbe888ea4abea68bb29d4b5635c0
boorimi <qnfal123@gmail.com>
2024-08-07 12:54:47 +0900

    [create] login check js in simulator-main.js

--- src/main/resources/static/js/simulator_main.js
+++ src/main/resources/static/js/simulator_main.js
@@ -1,4 +1,40 @@
 $(document).ready(function () {
+    checkJwtCookie();
+
+
+    // 쿠키에서 특정 이름의 값을 가져오는 함수
+    function getCookie(name) {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // 쿠키의 이름이 매칭되는지 확인
+            if (cookie.indexOf(name + '=') === 0) {
+                return cookie.substring((name + '=').length, cookie.length);
+            }
+        }
+        return null;
+    }
+
+    // JWT 쿠키가 있는지 확인하는 함수
+    function checkJwtCookie() {
+        var jwt = getCookie('jwt'); // 쿠키 이름이 'jwt'라고 가정
+        if (jwt) {
+            // console.log('JWT 쿠키가 존재합니다:', jwt);
+            $('.simulator-create-project').append(`<div class="create-project" onclick="location.href='/simulator'">+</div>`);
+        } else {
+            // console.log('JWT 쿠키가 존재하지 않습니다.');
+            $('.simulator-create-project').append(`<div class="project-login">
+            <p>Available after login</p>
+            <a href="/loginC">Google Login</a>
+        </div>`);
+
+
+
+        }
+    }
+
+
+    // 좌우 드래그 스크롤
     let isDown = false;
     let startX;
     let scrollLeft;
