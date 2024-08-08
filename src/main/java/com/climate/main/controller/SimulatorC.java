2846685cc30fea008f5b8f97bc9ce8aa639a1a48
boorimi <qnfal123@gmail.com>
2024-08-08 12:06:01 +0900

    [edit] save test

--- src/main/java/com/climate/main/controller/SimulatorC.java
+++ src/main/java/com/climate/main/controller/SimulatorC.java
@@ -4,9 +4,15 @@ import com.climate.main.service.HoldDAO;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RequestMapping("/simulator")
 @Controller
@@ -42,6 +48,29 @@ public class SimulatorC {
     public String simulatorGallery(Model model){
         model.addAttribute("content", "/simulator/simulator_gallery");
         return "index";
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        String projectPath = System.getProperty("user.dir") + "/src/main/resources/static/upload/s_project/";
+        String res = "";
+
+        try {
+            // 파일 객체 생성                                 //업로드된 파일의 원본 이름
+            File targetFile = new File(projectPath + file.getOriginalFilename());
+            System.out.println("패치: "+projectPath);
+            // 파일 저장
+            file.transferTo(targetFile);
+            System.out.println("저장 성공");
+
+            res = "success";
+            return ResponseEntity.ok(res);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            res = "fail";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+        }
     }
 
 
