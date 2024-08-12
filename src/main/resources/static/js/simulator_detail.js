4134d0f20cb504f2043d600e88d13452d02dc6a0
boorimi <qnfal123@gmail.com>
2024-08-12 19:51:41 +0900

    [edit] simulator detail page / three js init success

---
+++ src/main/resources/static/js/simulator_detail.js
@@ -0,0 +1,184 @@
+import * as THREE from "three";
+import {OrbitControls} from "three/addons/controls/OrbitControls.js";
+import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
+
+$(document).ready(function () {
+    // let fileName = $('.content-container').data('file');
+    // console.log(fileName);
+});
+
+function rgbToHex(r, g, b) {
+    return (r << 16) | (g << 8) | b;
+}
+
+class App {
+    constructor() {
+        this._divContainer = document.querySelector("#webgl-container");
+        this._renderer = new THREE.WebGLRenderer({antialias: true});
+        this._scene = new THREE.Scene();
+        this._raycaster = new THREE.Raycaster();
+        this._mouse = new THREE.Vector2();
+        this._camera = new THREE.PerspectiveCamera();
+        this._controls = null;
+    }
+
+    initialize() {
+        init(this);
+    }
+
+    resize() {
+        resize(this);
+    }
+
+    render(time) {
+        render(this, time);
+    }
+
+    update(time) {
+        update(this, time);
+    }
+
+
+}
+
+function init(app) {
+    app._renderer.setPixelRatio(window.devicePixelRatio);
+    app._divContainer.appendChild(app._renderer.domElement);
+
+    const scene = app._scene;
+    let rgbColor = rgbToHex(226, 240, 215);
+    scene.background = new THREE.Color(rgbColor);
+
+    setupCamera(app);
+    setupLight(app);
+    setupModel(app);
+    setupControls(app);
+
+    app.resize();
+
+    window.onresize = () => app.resize();
+
+    requestAnimationFrame((time) => app.render(time));
+
+    console.log('width', window.innerWidth);
+    console.log('height', window.innerHeight);
+
+}
+
+function resize(app) {
+    const width = app._divContainer.clientWidth;
+    const height = app._divContainer.clientHeight;
+    app._camera.aspect = width / height;
+    app._camera.updateProjectionMatrix();
+
+    app._renderer.setSize(width, height);
+}
+
+function render(app, time) {
+    requestAnimationFrame((time) => app.render(time));
+    app._renderer.render(app._scene, app._camera);
+    app.update(time);
+}
+
+function update(app, time) {
+    time *= 0.001;
+}
+
+function setupCamera(app) {
+    const width = app._divContainer.clientWidth;
+    const height = app._divContainer.clientHeight;
+    const camera = new THREE.PerspectiveCamera(65, width / height, 0.1, 1000);
+    camera.position.z = 7;
+    app._camera = camera;
+}
+
+function setupLight(app) {
+    let rgbColor = rgbToHex(255, 255, 255);
+    const intensity = 1;
+    const directionalLight = new THREE.DirectionalLight(rgbColor, intensity);
+    directionalLight.position.set(-3, 4.7, 4);
+    app._scene.add(directionalLight);
+
+    const ambientLight = new THREE.AmbientLight(rgbColor, 1.5);
+    app._scene.add(ambientLight);
+
+    const pointLight = new THREE.PointLight(rgbColor, 50);
+    pointLight.position.set(0, 5, 5);
+    app._scene.add(pointLight);
+}
+
+function setupModel(app) {
+    const loader = new GLTFLoader();
+    const fileName = $('.content-container').data('file');
+    const filePath = `/resources/upload/s_project/3D/${fileName}`;
+
+    loader.load(
+        filePath,
+        function (gltf) {
+            const model = gltf.scene;
+            app._scene.add(model);
+
+            // 모델 크기 조정 (옵션)
+            model.scale.set(1, 1, 1);
+
+            // 모델 위치 조정 (옵션)
+            model.position.set(0, 0, 0);
+        },
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        },
+        function (error) {
+            console.error('An error happened', error);
+        }
+    );
+}
+
+// OrbitControls를 설정하여 카메라 조작
+// 카메라의 팬(pan) 범위를 제한하는 로직 추가
+function setupControls(app) {
+    const controls = new OrbitControls(app._camera, app._divContainer);
+
+    // 패닝 한계 설정
+    controls.minPolarAngle = -Math.PI / 2;
+    controls.maxPolarAngle = Math.PI / 2;
+    controls.minAzimuthAngle = -Math.PI / 2;
+    controls.maxAzimuthAngle = Math.PI / 4;
+    controls.screenSpacePanning = true;
+    const maxPan = 2;
+    const minPan = -2;
+
+    function constrainPan() {
+        const offset = controls.target.clone();
+
+        if (offset.x > maxPan) {
+            offset.x = maxPan;
+        } else if (offset.x < minPan) {
+            offset.x = minPan;
+        }
+
+        if (offset.y > maxPan) {
+            offset.y = maxPan;
+        } else if (offset.y < minPan) {
+            offset.y = minPan;
+        }
+
+        if (offset.z > maxPan) {
+            offset.z = maxPan;
+        } else if (offset.z < minPan) {
+            offset.z = minPan;
+        }
+
+        controls.target.copy(offset);
+    }
+
+    controls.addEventListener("change", constrainPan);
+    controls.maxDistance = 15;
+    app._controls = controls;
+}
+
+
+let app;
+window.onload = function () {
+    app = new App();
+    app.initialize();
+};
