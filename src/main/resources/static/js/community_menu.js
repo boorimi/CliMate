3d9406cdc8ec608d761a383a99931fbb9aa96726
boorimi <qnfal123@gmail.com>
2024-08-14 19:16:53 +0900

    [edit] community video css

--- src/main/resources/static/js/community_menu.js
+++ src/main/resources/static/js/community_menu.js
@@ -1,23 +1,25 @@
 $(document).ready(function () {
+
     const searchOpenIcon = $(".community-search-icon");
     const searchMenu = $(".community-search-wrapper");
 
-    searchOpenIcon.append('<img src="/resources/icon/search.png">');
+    searchOpenIcon.append('<img src="/resources/icon/search_green.png">');
 
     // 검색 인풋 오픈
     searchOpenIcon.click(function () {
         searchMenu.slideToggle();
         searchMenu.css("display", "flex");
 
-        const iconImg = searchOpenIcon.find('img');
+        const iconImg = searchOpenIcon.find("img");
         // 아이콘 토글
-        if (iconImg.attr('src') === '/resources/icon/search.png') {
-            iconImg.attr('src', '/resources/icon/close.png');
-            iconImg.css('width', '50%');
+        if (iconImg.attr("src") === "/resources/icon/search_green.png") {
+            iconImg.attr("src", "/resources/icon/close.png");
+            iconImg.css("height", "60%");
         } else {
-            iconImg.attr('src', '/resources/icon/search.png');
-            iconImg.css('width', '100%');
+            iconImg.attr("src", "/resources/icon/search_green.png");
+            iconImg.css("height", "100%");
         }
     });
 
 });
+
