667af7b55a701738d30ba5d6121b8026231f318a
boorimi <qnfal123@gmail.com>
2024-08-16 02:59:30 +0900

    [edit] community lfg insert css

--- src/main/resources/static/js/community_video.js
+++ src/main/resources/static/js/community_video.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 
-    updateText();
     changeColor();
 
     function changeColor(){
@@ -10,43 +9,5 @@ $(document).ready(function () {
         });
     }
 
-    function updateText() {
-
-        $('.community-climbing-name').each(function () {
-            let text = $(this).text();
-            let english = /[qwertyuiopasdfghjklzxcvbnm]/.test(text);
-
-            if ($(window).width() <= 768) {
-                if (english) {
-                    if (text.length > 15) {
-                        let moreText = text.substring(0, 15) + '..';
-                        $(this).text(moreText);
-                    }
-                } else {
-                    if (text.length > 7) {
-                        let moreText = text.substring(0, 7) + '..';
-                        $(this).text(moreText);
-                    }
-                }
-            } else {
-                if (english) {
-                    if (text.length > 30) {
-                        let moreText = text.substring(0, 30) + '..';
-                        $(this).text(moreText);
-                    }
-                } else {
-                    if (text.length > 11) {
-                        let moreText = text.substring(0, 11) + '..';
-                        $(this).text(moreText);
-                    }
-                }
-            }
-        });
-    }
-
-// 창 크기 변경 시마다 실행
-    $(window).resize(function () {
-        updateText();
-    });
 
 });
