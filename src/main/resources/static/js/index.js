0f4d51e86a74ca2c0271d4b5aeefee4829dede2b
boorimi <qnfal123@gmail.com>
2024-08-20 17:28:14 +0900

    [edit] modify

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -95,7 +95,62 @@ $(document).ready(function () {
       "/community/searchLfg?columnName=u_nickname&searchWord=" +
       currentNickname;
   });
+
+
+  // 로그인 필요 모달
+  $(".s-menu-create").click(function (){
+    if (userId != null && userId != ""){
+      location.href="/simulator/create";
+    } else {
+      showConfirm();
+    }
+  })
+
+  $(".s-menu-my-project").click(function (){
+    if (userId != null && userId != ""){
+      location.href="/simulator/my_project";
+    } else {
+      showConfirm();
+    }
+  })
+
+  $(".s-menu-gallery").click(function (){
+    location.href="/simulator/gallery?category=All";
+  })
+
+  function loginCheck(userId, dataPk) {
+    if (userId != null && userId !== '') {
+      location.href = '/simulator/gallery_detail?b_pk=' + dataPk;
+    } else {
+      showConfirm();
+    }
+  }
+
 });
+
+// 로그인 필요 모달
+function showConfirm() {
+  document.getElementById('s-menu-modal-background').style.display = 'block';
+}
+
+// 로그인 버튼 클릭
+document.getElementById('s-menu-confirm-yes').addEventListener('click', function() {
+  document.getElementById('s-menu-modal-background').style.display = 'none';
+  location.href = '/loginC';
+});
+
+// 취소 버튼 클릭
+document.getElementById('s-menu-confirm-no').addEventListener('click', function() {
+  document.getElementById('s-menu-modal-background').style.display = 'none';
+});
+
+// 배경을 클릭했을 때 모달 닫기
+document.getElementById('s-menu-modal-background').addEventListener('click', function(event) {
+  if (event.target === this) {
+    this.style.display = 'none';
+  }
+});
+
 
 function toggleMenu() {
   document.querySelector(".hamburger-menu").classList.toggle("active");
--- src/main/resources/static/js/simulator_menu.js
+++ src/main/resources/static/js/simulator_menu.js
@@ -1,56 +1,5 @@
 $(document).ready(function () {
-    let userId = $('#userId').text();
 
-
-    $(".s-menu-create").click(function (){
-        if (userId != null && userId != ""){
-            location.href="/simulator/create";
-        } else {
-            showConfirm();
-        }
-    })
-
-    $(".s-menu-my-project").click(function (){
-        if (userId != null && userId != ""){
-            location.href="/simulator/my_project";
-        } else {
-            showConfirm();
-        }
-    })
-
-    $(".s-menu-gallery").click(function (){
-        location.href="/simulator/gallery?category=All";
-    })
-
-    function loginCheck(userId, dataPk) {
-        if (userId != null && userId !== '') {
-            location.href = '/simulator/gallery_detail?b_pk=' + dataPk;
-        } else {
-            showConfirm();
-        }
-    }
 })
 
-// 모달
-function showConfirm() {
-    document.getElementById('s-menu-modal-background').style.display = 'block';
-}
-
-// 로그인 버튼 클릭
-document.getElementById('s-menu-confirm-yes').addEventListener('click', function() {
-    document.getElementById('s-menu-modal-background').style.display = 'none';
-    location.href = '/loginC';
-});
-
-// 취소 버튼 클릭
-document.getElementById('s-menu-confirm-no').addEventListener('click', function() {
-    document.getElementById('s-menu-modal-background').style.display = 'none';
-});
-
-// 배경을 클릭했을 때 모달 닫기
-document.getElementById('s-menu-modal-background').addEventListener('click', function(event) {
-    if (event.target === this) {
-        this.style.display = 'none';
-    }
-});
 
